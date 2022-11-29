5b0dafa82fa23542b40f04fc59e344230d6903ce
Iadityaraj21 <mishraadityaraj221@gmail.com>
2022-11-29 20:50:30 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,107 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+           Video games are the current rend and people of all ages are behind
+         the various kinds of video games available today.A video game is an
+          electronic game which involves a human interaction with a user interface,
+           which would generate a visual feedback onto a video device.The electronic
+            systems that are used to play the games are known as platforms and it could
+             range from small hand held devices to large mainframe computers.
+             Video games could have both negative and positive impact on children 
+             especially. The positive side involves where the children could develop 
+             their thinking skills and stimulate their learning facts like strategic thinking,
+              cooperation, creative thinking, and also innovative thinking, which is a very
+               important part in the information society!
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Future Is Gaming
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Gaming On Comsole
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Night Life Is Cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
